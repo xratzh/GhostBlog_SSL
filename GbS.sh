4704f97a3ec293950cf30cb5546cc80eb3cb2927
Xrat Zhou <xratzh@gmail.com>
2017-02-07 22:36:24 +0800

    Create GbS.sh

---
+++ GbS.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Check to make sure script is being run as root
+if [[ `whoami` != root ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+#Check to see what OS is being used and start run the scripts
+
+/bin/uname -a > /tmp/osversion.txt
+
+if grep "Ubuntu" "/tmp/osversion.txt" > /dev/null; then
+    echo "Ubuntu"
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
+    sudo sh deb.sh
+    rm deb.sh
+elif grep "SMP" "/tmp/osversion.txt" > /dev/null; then
+    echo "CentOS"
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
+    sudo sh yum.sh
+    rm yum.sh
+elif grep "Debian" "/tmp/osversion.txt" > /dev/null; then
+    echo "Mac OS X"
+fi
