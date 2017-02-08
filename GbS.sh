58876d7900154f9343d2d3c8cb5b7b3780afdc79
Xrat Zhou <xratzh@gmail.com>
2017-02-08 20:36:48 +0800

    Update GbS.sh

--- GbS.sh
+++ GbS.sh
@@ -5,11 +5,7 @@ if [[ -f /etc/redhat-release ]]; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
     sudo sh yum.sh
     rm yum.sh
-elif cat /etc/issue | grep -Eqi "debian"; then
-    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
-    sudo sh deb.sh
-    rm deb.sh
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
+elif cat /etc/issue | grep -Eqi "debian|ubuntu"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
     sudo sh deb.sh
     rm deb.sh
@@ -17,11 +13,7 @@ elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
     sudo sh yum.sh
     rm yum.sh
-elif cat /proc/version | grep -Eqi "debian"; then
-    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
-    sudo sh deb.sh
-    rm deb.sh
-elif cat /proc/version | grep -Eqi "ubuntu"; then
+elif cat /proc/version | grep -Eqi "debian|ubuntu"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
     sudo sh deb.sh
     rm deb.sh
