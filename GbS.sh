c1fc5baf089b0af8fbef7b0197be1cf9ff79a00d
Xrat Zhou <xratzh@gmail.com>
2017-02-08 20:20:06 +0800

    Update GbS.sh

--- GbS.sh
+++ GbS.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-#Check to make sure script is being run as root
-if [[ `whoami` != root ]]; then
-    echo "This script must be run as root"
-    exit 1
-fi
-
-#Check to see what OS is being used and start run the scripts
-
-/bin/uname -a > /tmp/osversion.txt
-
-if grep "Ubuntu" "/tmp/osversion.txt" > /dev/null; then
-    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
-    sudo sh deb.sh
-    rm deb.sh
-elif grep "SMP" "/tmp/osversion.txt" > /dev/null; then
+#Check system
+if [[ -f /etc/redhat-release ]]; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
     sudo sh yum.sh
     rm yum.sh
-elif grep "Debian" "/tmp/osversion.txt" > /dev/null; then
-    echo "Mac OS X"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
+    sudo sh deb.sh
+    rm deb.sh
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
+    sudo sh deb.sh
+    rm deb.sh
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
+    sudo sh yum.sh
+    rm yum.sh
+elif cat /proc/version | grep -Eqi "debian"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
+    sudo sh deb.sh
+    rm deb.sh
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
+    sudo sh deb.sh
+    rm deb.sh
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
+    sudo sh yum.sh
+    rm yum.sh
 fi
