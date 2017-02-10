12fae154aa4258b080e81402701686b9b92a2b9b
Xrat Zhou <xratzh@gmail.com>
2017-02-10 13:35:23 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -138,7 +138,7 @@ service nginx restart
 # add a crontab job
 
 cat >> /var/spool/cron/root <<EOF
-0 0 1 */2 * /opt/certbot-auto renew --quiet --no-self-upgrade --pre-hook "service nginx stop" --post-hook "service nginx start"
+0 0 1 */2 * /opt/certbot-auto renew --quiet --no-self-upgrade
 EOF
 
 clear
