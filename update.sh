5c18300d303b4342ecb96e69eda5093b6832769e
Xrat Zhou <xratzh@gmail.com>
2017-03-21 22:28:28 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "This will restart your nodejs services!"
+
+#stop nodejs with forever
+forever stop /var/www/ghost/index.js
+
+#download the latest version of Ghost
 cd /var/www/ghost
 tar zcvf content.tar.gz content
 mv content.tar.gz ../
@@ -10,7 +16,14 @@ unzip -uo ghost.zip -d ghost
 rm -rf ghost.zip
 chmod 755 /var/www/ghost
 cd ghost
+
+#install the latest Ghost version
 npm install --production
 rm -rf /var/www/ghost/content
 mv ../content.tar.gz . && tar zxvf content.tar.gz && rm content.tar.gz
 rm /var/www/ghost/config.example.js && mv ../config.js .
+
+#start nodejs and your Ghost blog
+forever start /var/www/ghost/index.js
+
+echo "Your update has been done!Enjoy the latest version"
