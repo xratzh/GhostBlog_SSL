b0379fc126683095b29d6425ded022dcd1a63878
Xrat Zhou <xratzh@gmail.com>
2017-07-04 21:37:09 +0800

    Update GbS.sh

--- GbS.sh
+++ GbS.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
-#Check system
+#Check system and install
 if [[ -f /etc/redhat-release ]]; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
-    sudo sh yum.sh
+    sudo bash yum.sh
     rm yum.sh
 elif cat /etc/issue | grep -Eqi "debian|ubuntu"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
-    sudo sh deb.sh
+    sudo bash deb.sh
     rm deb.sh
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
-    sudo sh yum.sh
+    sudo bash yum.sh
     rm yum.sh
 elif cat /proc/version | grep -Eqi "debian|ubuntu"; then
-    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh  
-    sudo sh deb.sh
+    wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/deb.sh
+    sudo bash deb.sh
     rm deb.sh
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     wget https://raw.githubusercontent.com/xratzh/GhostBlog_SSL/master/yum.sh  
-    sudo sh yum.sh
+    sudo bash yum.sh
     rm yum.sh
 fi
