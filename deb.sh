a90bbb3f08f937f1694cc0e5dd5e153d9675b78d
Xrat Zhou <xratzh@gmail.com>
2017-07-31 11:49:43 +0800

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -35,7 +35,7 @@ apt-get install -y nodejs
 mkdir /var/www
 cd /var/www
 rm -rf ghost
-curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
+curl -L https://github.com/TryGhost/Ghost/releases/download/0.11.11/Ghost-0.11.11.zip -o ghost.zip
 unzip -uo ghost.zip -d ghost
 rm -rf ghost.zip
 chmod 755 /var/www/ghost
