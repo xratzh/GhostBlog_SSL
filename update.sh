765fcc23986de61d1d40dd1a1384083d3828d7d9
Xrat Zhou <xratzh@gmail.com>
2017-08-06 12:27:14 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,25 +5,54 @@ echo "This will restart your ghost/index.js services!"
 #stop nodejs with forever
 forever stop /var/www/ghost/index.js
 
-#download the latest version of Ghost
+#update to the latest version of Ghost
 cd /var/www/ghost
 tar zcvf content.tar.gz content
 mv content.tar.gz ../
 mv config.js ../
-cd .. && rm -rf ghost
-curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
-unzip -uo ghost.zip -d ghost
-rm -rf ghost.zip
-chmod 755 /var/www/ghost
-cd ghost
+mkdir ghost && cd ghost
+npm i -g ghost-cli
 
 #install the latest Ghost version
-npm install --production
+ghost install local
 rm -rf /var/www/ghost/content
 mv ../content.tar.gz . && tar zxvf content.tar.gz && rm content.tar.gz
-rm /var/www/ghost/config.example.js && mv ../config.js .
 
 #start nodejs and your Ghost blog
-forever start /var/www/ghost/index.js
+ghost restart
 
+#update bootup settins
+if [[ -f /etc/redhat-release ]]; then
+    sed -i '/forever start \/var\/www\/ghost\/index.js/d' /etc/rc.d/rc.local
+    sed -i '/exit 0/d' /etc/rc.d/rc.local
+    echo "cd /var/www/ghost && ghost start" >> /etc/rc.d/rc.local
+    echo "exit 0" >> /etc/rc.d/rc.local
+    chmod +x /etc/rc.d/rc.local
+    
+elif cat /etc/issue | grep -Eqi "debian|ubuntu"; then
+    sed -i '/forever start \/var\/www\/ghost\/index.js/d' /etc/rc.local
+    sed -i '/exit 0/d' /etc/rc.local
+    echo "cd /var/www/ghost && ghost start" >> /etc/rc.local
+    echo "exit 0" >> /etc/rc.local
+
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    sed -i '/forever start \/var\/www\/ghost\/index.js/d' /etc/rc.d/rc.local
+    sed -i '/exit 0/d' /etc/rc.d/rc.local
+    echo "cd /var/www/ghost && ghost start" >> /etc/rc.d/rc.local
+    echo "exit 0" >> /etc/rc.d/rc.local
+    chmod +x /etc/rc.d/rc.local
+    
+elif cat /proc/version | grep -Eqi "debian|ubuntu"; then
+    sed -i '/forever start \/var\/www\/ghost\/index.js/d' /etc/rc.local
+    sed -i '/exit 0/d' /etc/rc.local
+    echo "cd /var/www/ghost && ghost start" >> /etc/rc.local
+    echo "exit 0" >> /etc/rc.local
+    
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    sed -i '/forever start \/var\/www\/ghost\/index.js/d' /etc/rc.d/rc.local
+    sed -i '/exit 0/d' /etc/rc.d/rc.local
+    echo "cd /var/www/ghost && ghost start" >> /etc/rc.d/rc.local
+    echo "exit 0" >> /etc/rc.d/rc.local
+    chmod +x /etc/rc.d/rc.local
+    
 echo "Your update has been done!Enjoy the latest version"
